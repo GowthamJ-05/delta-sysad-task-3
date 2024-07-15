cb2720cdd57e45b80527e0134ec1d13586198c28
GowthamJ-05 <141749637+GowthamJ-05@users.noreply.github.com>
2024-07-15 23:59:10 +0530

    Update embedimage.sh

--- task3b/forensics/embedimage.sh
+++ task3b/forensics/embedimage.sh
@@ -6,4 +6,4 @@ echo "Congrats! Here's your flag" > flag.txt
 
 base64 < flag.txt > encoded_text.txt
 convert mystery.png -set comment "$(cat encoded_text.txt)" mystery_with_text.png
-exiftool -comment output_with_text.png
+exiftool -comment mystery_with_text.png
