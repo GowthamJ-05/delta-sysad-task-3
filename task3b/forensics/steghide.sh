31363dcd07f32e9bca93d8ae8aaa6920e0e35079
GowthamJ-05 <141749637+GowthamJ-05@users.noreply.github.com>
2024-07-15 23:57:35 +0530

    Update steghide.sh

--- task3b/forensics/steghide.sh
+++ task3b/forensics/steghide.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Installing steghide
-sudo apt-get install -y steghide 
-
-# Check installation
-steghide
-
-# Create the text file to be embedded 
-touch "flag.txt"
-echo "Congrats! Here's your flag" > flag.txt
-
-# Embed the file using steghide
-steghide embed -ef flag.txt -ef mystery.png
-
-# Check whether a file is embedded or not
-steghide info mystery.png
-
-# Extract the data
-steghide extract -sf mystery.png
+base64 < your_text_file.txt > encoded_text.txt
+convert mystery.png -set comment "$(cat encoded_text.txt)" mystery_with_text.png
+exiftool -comment output_with_text.png
